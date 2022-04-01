19a457e4d09bdc6e6f061110b7de618492031c8d
BabyJIAN <85269099+2290653824@users.noreply.github.com>
2022-04-01 10:54:43 +0800

    Add files via upload

---
+++ springsecuritydemo/securitytest.sql
@@ -0,0 +1,31 @@
+/*
+MySQL Backup
+Database: securitytest
+Backup Time: 2022-04-01 10:53:01
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `securitytest`.`sys_user`;
+CREATE TABLE `sys_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_name` varchar(64) NOT NULL DEFAULT 'NULL' COMMENT '用户名',
+  `nick_name` varchar(64) NOT NULL DEFAULT 'NULL' COMMENT '昵称',
+  `password` varchar(64) NOT NULL DEFAULT 'NULL' COMMENT '密码',
+  `status` char(1) DEFAULT '0' COMMENT '账号状态（0正常 1停用）',
+  `email` varchar(64) DEFAULT NULL COMMENT '邮箱',
+  `phonenumber` varchar(32) DEFAULT NULL COMMENT '手机号',
+  `sex` char(1) DEFAULT NULL COMMENT '用户性别（0男，1女，2未知）',
+  `avatar` varchar(128) DEFAULT NULL COMMENT '头像',
+  `user_type` char(1) NOT NULL DEFAULT '1' COMMENT '用户类型（0管理员，1普通用户）',
+  `create_by` bigint DEFAULT NULL COMMENT '创建人的用户id',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint DEFAULT NULL COMMENT '更新人',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `del_flag` int DEFAULT '0' COMMENT '删除标志（0代表未删除，1代表已删除）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
+BEGIN;
+LOCK TABLES `securitytest`.`sys_user` WRITE;
+DELETE FROM `securitytest`.`sys_user`;
+UNLOCK TABLES;
+COMMIT;
